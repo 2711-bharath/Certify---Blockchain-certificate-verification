9ea941688473d3680781673ae1a2886afa91a967
Bharath Reddy Madha <54592001+2711-bharath@users.noreply.github.com>
2023-01-21 11:32:37 +0530

    fix: clean

--- frontend/src/route/index.js
+++ frontend/src/route/index.js
@@ -1,8 +1,5 @@
 import VueRouter from "vue-router";
 import Vue from "vue";
-import Alerts from "../modules/alerts/pages/Alerts.vue";
-import AlertDetails from "../modules/alerts/pages/AlertDetails.vue";
-import Dashboard from "../modules/dashboard/pages/Dashboard.vue";
 import LandingPage from "../modules/landing-page/pages/landing-page.vue";
 import Login from "../modules/authentication/login/pages/login.vue";
 import Signup from "../modules/authentication/signup/pages/signup.vue";
@@ -26,21 +23,6 @@ const routes = [
     component: Signup,
   },
   {
-    path: "/admin",
-    name: "dashboard",
-    component: Dashboard,
-  },
-  {
-    path: "/alerts",
-    name: "alerts",
-    component: Alerts,
-  },
-  {
-    path: "/alerts/:alert_id",
-    name: "alert-details",
-    component: AlertDetails,
-  },
-  {
     path: "*",
     redirect: "/",
   },
@@ -54,10 +36,10 @@ const router = new VueRouter({
 router.beforeEach((to, from, next) => {
   const isLoggedIn = Boolean(localStorage.getItem("user"));
   if (isLoggedIn) {
-    if (to.name === "Login" || to.name === "Register") next("/view-board");
+    if (to.name === "login" || to.name === "signup") next("/dashboard");
     else next();
   } else if (["dashboard"].includes(to.name)) {
-    next("/register");
+    next("/sign-up");
   } else {
     next();
   }
