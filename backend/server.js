a7b376d8f5d9cd497df9557b1d1e4409dd34ef7e
Bharath Reddy Madha <54592001+2711-bharath@users.noreply.github.com>
2023-02-09 23:03:10 +0530

    fix: port

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const { connect } = require('./DB/connect');
-const userRouter = require('./routes/userRoute');
+const express = require("express");
+const { connect } = require("./DB/connect");
+const userRouter = require("./routes/userRoute");
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/user', userRouter);
+app.use("/user", userRouter);
 
-app.get('/', (req, res) => {
-    res.send('Welcome to Blockchain Certificate Storage Backend');
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to Blockchain Certificate Storage Backend");
+});
 
-app.listen(3000, async () => {
-    console.log('listening to port 3000');
-    await connect();
+app.listen(process.env.PORT || 3000, async () => {
+  console.log("listening to port 3000");
+  await connect();
 });
