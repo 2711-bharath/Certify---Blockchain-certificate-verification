c01e578a02ef762505dbf6e4ff57d7ed4dd154fe
Bharath Reddy Madha <54592001+2711-bharath@users.noreply.github.com>
2023-02-11 20:06:55 +0530

    backend final authentication

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
-const express = require('express');
-const { connect } = require('./DB/connect');
-const userRouter = require('./routes/userRoute');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { connect } = require("./DB/connect");
+const userRouter = require("./routes/userRoute");
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: true }));
 
-app.use('/user', userRouter);
+app.use("/user", userRouter);
 
 app.listen(3000, async () => {
-    console.log('listening to port 3000');
-    await connect();
+  console.log("listening to port 3000");
+  await connect();
 });
