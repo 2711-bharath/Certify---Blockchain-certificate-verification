46c4a9ef965a589a03878a4e78765babf271e62e
Bharath Reddy Madha <54592001+2711-bharath@users.noreply.github.com>
2023-02-11 21:16:49 +0530

    Revert "fix: remove frontend folder in backend"
    This reverts commit 4f3bd92f80db7b703ce3d0b0e9175a59ad4953b1.

---
+++ frontend/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Header />
+    <div class="columns is-gapless">
+      <Sidebar v-if="is_logged_in" />
+      <div class="column is-size-1 has-text-centered main">
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from "./shared/header/Header.vue";
+  import Sidebar from "./shared/sidebar/Sidebar.vue";
+
+  export default {
+    name: "App",
+    components: {
+      Sidebar,
+      Header,
+    },
+    computed: {
+      is_logged_in() {
+        return Boolean(localStorage.getItem("user"));
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .main {
+    position: relative;
+  }
+</style>
---
+++ frontend/src/apis/api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => config,
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
